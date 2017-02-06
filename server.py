27087acbb317c1d63b96e77ed133c120ee6b1a7c
Navjot Singh <weavebytes@gmail.com>
2017-02-06 11:49:56 +0000

    introducing templates

--- server.py
+++ server.py
@@ -2,12 +2,17 @@
 # main script for starting server
 #
 
-from flask import Flask
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    templateData = { 'year' : 2017 }
+
+    # pass the template data into the template index.html and
+    # return it to the user
+    return render_template('index.html', **templateData)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
