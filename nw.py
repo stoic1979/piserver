5bb52e3c145628ec5b80b013f9e22069828481c2
Navjot Singh <weavebytes@gmail.com>
2017-02-07 19:05:38 +0530

    added note for wifi dev name

--- nw.py
+++ nw.py
@@ -12,14 +12,18 @@ def get_wifis(dev='wlan0'):
     """
     function scans wifi nws on given device
     and retuns the list of ssids
+
     """
     ssids = []
+
+    # NOTE: if you cant get list of ssids,
+    # probably, you will need to specify appropriate dev name
     cells = Cell.all(dev)
+
     for cell in cells:
         ssids.append(cell.ssid)
 
     return ssids
-
 
 if __name__ == "__main__":
     print get_wifis('wlp3s0')
