87fcb45e521938cbe02d93d6040ffff57103737d
Navjot Singh <weavebytes@gmail.com>
2017-02-07 19:21:51 +0530

    show wifi is ui

--- server.py
+++ server.py
@@ -3,6 +3,7 @@
 #
 
 from flask import Flask, render_template, request
+from nw import *
 
 app = Flask(__name__)
 
@@ -13,6 +14,12 @@ def hello():
     # pass the template data into the template index.html and
     # return it to the user
     return render_template('index.html', **templateData)
+
+@app.route("/wifi")
+def hello():
+    templateData = { 'wifis' : get_wifis() }
+
+    return render_template('wifi.html', **templateData)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
