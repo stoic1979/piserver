4cd380f1c2a65363c79a7ad080e9984218c30bda
Navjot Singh <weavebytes@gmail.com>
2017-02-10 15:16:42 +0530

    adding setup file

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#
+# setup file for dependencies
+#
+
+sudo apt-get install python-dev
+sudo apt-get install libbluetooth-dev
+sudo pip install flask
+sudo pip install wifi
+sudo pip install PyBluez
